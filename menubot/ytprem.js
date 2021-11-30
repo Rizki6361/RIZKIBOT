2b11d1b577c10b434fba2beb65d4fcb75cd9daf7
Rizki636 <pratamarizkiridho0@gmail.com>
2021-11-30 13:58:13 +0800

    RIZKIBOT

---
+++ menubot/ytprem.js
@@ -0,0 +1,18 @@
+const jytprem = (prefix) => {
+return`━━֍  〔 🪀 _*BY ${botName}*_ 🪀〕
+──────────────────
+◈ _Nama Owner : ${NamaOwner}_
+◈ _Nomor Owner : ${NomorOwner}_
+────────────────────
+━━֍〔 🪀 *JUAL YOUTUBE PREMIUM*🪀〕
+────────────────────
+◈ YouTube Premium 1 Bulan = 1k(akun kamu)
+◈ YouTube Premium 1 Bulan = 2k(akun admin)
+◈ YouTube Premium 4 Bulan = 3k(akun kamu)
+◈ YouTube Premium 4 Bulan = 4k(akun admin)
+────────────────────
+Minat Hubungi http://wa.me/6281256038434
+────────────────────
+`
+}
+exports.jytprem = jytprem
